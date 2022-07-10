4cae3fa49b4ffe2ad3e0e4956f31adea353a0399
Leonard Stutzer <stutzer.leonard@gmail.com>
2022-07-10 08:57:01 +0200

    udpate eval.sh, remove todos

--- evaluation_scripts/eval.sh
+++ evaluation_scripts/eval.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
-evalFile="$(date +%s)"
-timeFormat="| %M | %E |"
-timeOutput="--append -o ./../evaluation_results/${evalFile}"
+# save current dir
+prev_dir=$(pwd)
+# go to the directory of the script
+cd "$(dirname "$0")"
+
+# setup constants
+evalDir="$(date +%s)"
+timeFormat="%M, %E"
+timeOutput="--append -o ./../evaluation_results/${evalDir}"
 time_exec=/usr/bin/time
 zok_exec=~/.zokrates/bin/zokrates
 zok_dir=./../zok
 go_exec=go
 
+# create new eval dir
+mkdir ./../evaluation_results/${evalDir}
+
 # generate arguments
 echo "Generate arguments..."
 for i in {1..5}
 do
-    ${go_exec} run generate_zok_program_args.go 30001 $i > /dev/null 2>&1
+    if [[ ! -e ${zok_dir}/arguments/batch_verifier_$i ]]
+    then
+        ${go_exec} run generate_zok_program_args.go 30001 $i > /dev/null 2>&1
+    fi
 done
 echo "Done."
 
-echo "| batch size | memory (kbytes) |    time |" > ./../evaluation_results/${evalFile}_compile
-echo "|------------|----------------:|--------:|" >> ./../evaluation_results/${evalFile}_compile
-echo "| batch size | memory (kbytes) |    time |" > ./../evaluation_results/${evalFile}_setup
-echo "|------------|----------------:|--------:|" >> ./../evaluation_results/${evalFile}_setup
-echo "| batch size | memory (kbytes) |    time |" > ./../evaluation_results/${evalFile}_compute_witness
-echo "|------------|----------------:|--------:|" >> ./../evaluation_results/${evalFile}_compute_witness
-echo "| batch size | memory (kbytes) |    time |" > ./../evaluation_results/${evalFile}_generate_proof
-echo "|------------|----------------:|--------:|" >> ./../evaluation_results/${evalFile}_generate_proof
+echo "batch size,memory (kbytes),time" > ./../evaluation_results/${evalDir}/compile.xlsx
+echo "batch size,memory (kbytes),time" > ./../evaluation_results/${evalDir}/setup.xlsx
+echo "batch size,memory (kbytes),time" > ./../evaluation_results/${evalDir}/compute_witness.xlsx
+echo "batch size,memory (kbytes),time" > ./../evaluation_results/${evalDir}/generate_proof.xlsx
 
 # execute different batches
 for i in {1..5}
@@ -32,19 +40,34 @@ do
     echo "Compile"
     # 1. compile
     # 1.1 adjust batch_verification.zok file
-    sed -i "s/const u32 BATCH_SIZE = \d/const u32 BATCH_SIZE = $i/" ${zok_dir}/batch_verification.zok
+    sed -i "s/const u32 BATCH_SIZE = [[:digit:]]/const u32 BATCH_SIZE = $i/g" ${zok_dir}/batch_verification.zok
     # 1.2 compile
-    ${time_exec} ${timeOutput}_compile -f "|$i ${timeFormat}" ${zok_exec} compile -i ${zok_dir}/batch_verification.zok -o ${zok_dir}/output/batch_verifier_$i  >> ./../evaluation_results/${evalFile}_constraints
+    echo "BATCH SIZE $i" >> ./../evaluation_results/${evalDir}/constraints
+    for j in {1..5}
+    do
+        ${time_exec} ${timeOutput}/compile.xlsx -f "$i, ${timeFormat}" ${zok_exec} compile -i ${zok_dir}/batch_verification.zok -o ${zok_dir}/output/batch_verifier_$i  >> ./../evaluation_results/${evalDir}/constraints
+    done
 
     echo "Setup"
     # 2. setup
-    ${time_exec} ${timeOutput}_setup -f "|$i ${timeFormat}" ${zok_exec} setup -i ${zok_dir}/output/batch_verifier_$i -p ${zok_dir}/output/proving_key_batch_verifier_$i.key -v ${zok_dir}/output/verification_key_batch_verifier_$i.key
+    for j in {1..5}
+    do
+        ${time_exec} ${timeOutput}/setup.xlsx -f "$i, ${timeFormat}" ${zok_exec} setup -i ${zok_dir}/output/batch_verifier_$i -p ${zok_dir}/output/proving_key_batch_verifier_$i.key -v ${zok_dir}/output/verification_key_batch_verifier_$i.key #> /dev/null 2>&1
+    done
 
     echo "Compute-witness"
     # 3. compute-witness
-    ${time_exec} ${timeOutput}_compute_witness -f "|$i ${timeFormat}" ${zok_exec} compute-witness -i ${zok_dir}/output/batch_verifier_$i -o ${zok_dir}/output/witness_batch_verifier_$i -a $(< ${zok_dir}/arguments/batch_verifier_$i)
+    for j in {1..5}
+    do
+        ${time_exec} ${timeOutput}/compute_witness.xlsx -f "$i, ${timeFormat}" ${zok_exec} compute-witness -i ${zok_dir}/output/batch_verifier_$i -o ${zok_dir}/output/witness_batch_verifier_$i -a $(< ${zok_dir}/arguments/batch_verifier_$i) #> /dev/null 2>&1
+    done
 
     echo "generate-proof"
     # 4. generate-proof
-    ${time_exec} ${timeOutput}_generate_proof -f "|$i ${timeFormat}" ${zok_exec} generate-proof -i ${zok_dir}/output/batch_verifier_$i -w ${zok_dir}/output/witness_batch_verifier_$i -p ${zok_dir}/output/proving_key_batch_verifier_$i.key -j ${zok_dir}/output/proof_batch_verifier_$i
+    for j in {1..5}
+    do
+        ${time_exec} ${timeOutput}/generate_proof.xlsx -f "$i, ${timeFormat}" ${zok_exec} generate-proof -i ${zok_dir}/output/batch_verifier_$i -w ${zok_dir}/output/witness_batch_verifier_$i -p ${zok_dir}/output/proving_key_batch_verifier_$i.key -j ${zok_dir}/output/proof_batch_verifier_$i #> /dev/null 2>&1
+    done
 done
+
+cd ${prev_dir}
